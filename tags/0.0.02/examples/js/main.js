daef374329cbf970c75a28417b1afd99cc07bf39
nas <nas@303664cc-d822-4519-8d5f-3ab00ffa3208>
2016-02-18 16:30:05 +0000

    do not build

---
+++ tags/0.0.02/examples/js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by trunk on 18/01/16.
+ */
+requirejs.config({
+	baseUrl: 'examples/js',
+	paths: {
+		jquery: '/lib/jquery-2.2.0',
+
+		home: '/examples/js/home'
+	},
+	packages: [
+		{
+			name    : "validator",
+			location: '/src/js/',
+			main    : 'validator'
+		}
+	]
+});
+
+requirejs( [ 'jquery', 'home' ], function ( $ ) {
+	/*$(document ).ready(function(){
+		home.init();
+	});*/
+});
