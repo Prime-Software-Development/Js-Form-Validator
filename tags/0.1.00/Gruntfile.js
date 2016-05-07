90f117e6ec016d5a09e4777664eb41e87fe05c61
nas <nas@303664cc-d822-4519-8d5f-3ab00ffa3208>
2016-05-07 16:14:45 +0000

    Added Grunfile

---
+++ tags/0.1.00/Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function( grunt ) {
+	var fs = require( "fs" );
+
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		build: {
+			all: {
+				dest: "dist/validator.js",
+				minimum: [
+					"core",
+					"additional-methods"
+				],
+
+				// Exclude specified modules if the module matching the key is removed
+				removeWith: {
+				}
+			}
+		},
+		jshint: {
+			all: {
+				src: [
+					"src/**/*.js", "Gruntfile.js", "build/**/*.js", "dist/*.js"
+				],
+				options: {
+					jshintrc: true
+				}
+			}
+		}
+	});
+
+	// Load grunt tasks from NPM packages
+	require( "load-grunt-tasks" )( grunt );
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// Integrate Validator specific tasks
+	grunt.loadTasks( "build/tasks" );
+
+	// run jslint
+	grunt.registerTask( "lint", [ "jshint" ] );
+
+	grunt.registerTask( "dev", [ "build:*:*", "lint" ] );
+
+	grunt.registerTask( "default", [ "dev" ] );
+};
