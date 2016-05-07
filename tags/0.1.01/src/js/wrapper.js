3b586166d56b6509a150f8601fd29c102746a8c4
nas <nas@303664cc-d822-4519-8d5f-3ab00ffa3208>
2016-05-07 16:34:14 +0000

    Changed which files are copied

---
+++ tags/0.1.01/src/js/wrapper.js
@@ -0,0 +1,14 @@
+/*jshint unused:false */
+/*!
+ * Validator Jquery JavaScript Library plugin v@VERSION
+ *
+ * Date: @DATE
+ */
+
+define(['jquery'], function($) {
+
+// @CODE
+// build.js inserts compiled Validator here
+
+	return $.fn.validator;
+});
